8f4eae07a5335c6cef57854a57109c9c5ce6efd7
Annlyn <annlynanto@gmail.com>
2024-09-07 18:16:57 +0530

    basic filtering

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import RestCard from "./RestrauntCard";
 import dataObj from "../utils/mockdata";
 
 const Body = () => {
+  const [restList, setResList] = useState(dataObj);
+
+  const handleFilter = () => {
+    let filteredList = dataObj.filter((res) => res.info.avgRating > 4);
+    setResList(filteredList);
+  };
+
   return (
     <div id="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={handleFilter}>
+          Top Rated Restraunts
+        </button>
+      </div>
       <div className="cards__container">
-        {dataObj.map((element) => (
+        {restList.map((element) => (
           <RestCard key={element.info.id} resData={element} />
         ))}
       </div>
